aa39f3cc76d1c7e76d3a0ae48403a17d76282555
JunxianQu <isjunxianqu@gmail.com>
2021-11-16 23:49:13 -0800

    GET POST

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const {nanoid} = require('nanoid');
+
+require('dotenv').config();
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+urls.createIndex({ slug: 1}, {unique: true});
+// urls.createIndex({ name: 1 }, { unique: true });
 
 const app = express();
 
@@ -11,22 +20,63 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
 
-// app.get('/url', (req, res) => {
-//   //TODO: get a short url by id
-// })
+app.get('/:id', async (req, res) => {
+  const {id: slug} = req.params;
+  try{
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.redirect(`/?error=${slug} not found`);
+  } catch (error) {
+    res.redirect(`/?error=link not found`);
+  }
+});
 
-// app.get('/:id', (req, res) => {
-//   //TODO: redirect to url
-//   res.json({
-//     message: 'cdg.sh - Short Urls for your code garden'
-//   });
-// });
+const schema = yup.object().shape({
+  slug: yup.string().trim().matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
 
-// app.post('/url', (req, res) => {
-//   //TODO: creat a short url
-// });
+app.post('/url', async (req, res, next) => {
+  let {slug, url} = req.body;
+  try {
+    await schema.validate({
+      url,
+      slug,
+    })
+    if (!slug) {
+      slug = nanoid(5);
+    } 
+    else {
+      const existing = await urls.findOne({slug});
+      if (existing) {
+        throw new Error('Slug in use.');
+      }
+    }
+    slug = slug.toLowerCase();
+    const newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
+});
 
-
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? 'pancake stack' : error.stack,
+  })
+})
 
 const port = process.env.PORT || 1337;
 app.listen(port, () =>{
